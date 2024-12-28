a7391a4195d54394db315afefb0731e5acf37730
MNlegion <rkreuser30@gmail.com>
2024-12-28 16:42:53 -0500

    middleware backend express error handling

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -1,31 +1,43 @@
+const asyncHandler = require('express-async-handler');
 
 // @desc    Get all projects
 // @route   GET /api/projects
 // @access  Private
-const getProjects = (req, res) => {
+const getProjects = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Projects retrieved successfully!" });
-};
+});
 
 // @desc    Create New Project
 // @route   POST /api/projects
 // @access  Private
-const createProject = (req, res) => {
+
+// This function will be used to create a new project. It will check if 
+// the request body contains the project information. 
+// If not, it will return a 400 status code with an error message. 
+// If the request body contains the project information, it will return a 200 status code with a success message.
+// New Projects contain a title, text description of project etc. TBD in the future.
+const createProject = asyncHandler(async (req, res) => {
+    if(!req.body.text) {
+        res.status(400)
+        throw new Error("Please provide project information"); 
+    }
+
     res.status(200).json({ message: "Project created successfully!" });
-};
+});
 
 // @desc    Update Project
 // @route   PUT /api/projects/:id
 // @access  Private
-const updateProject = (req, res) => {
+const updateProject = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Project ${req.params.id} Updated Successfully!` });
-};
+});
 
 // @desc    Delete Project
 // @route   DELETE /api/projects/:id
 // @access  Private
-const deleteProject = (req, res) => {
+const deleteProject = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Project ${req.params.id} Deleted Successfully!`  });
-};
+});
 
 
 module.exports = { 
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const {errorHandler} = require('./middleware/errorMiddleware');
 const port = process.env.PORT || 5000;
+
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/api/projects', require('./routes/projectRoutes'));
+
+// Error Handler Middleware
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
