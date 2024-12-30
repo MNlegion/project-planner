abae8fdaf7e227cd08a6fe1875f9e74b5700fbde
MNlegion <rkreuser30@gmail.com>
2024-12-29 20:15:30 -0500

    mongoDB connection setup

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,28 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const {errorHandler} = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
 
+// Connect to MongoDB 
+connectDB();
+
+// Initialize Express
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use('/api/projects', require('./routes/projectRoutes'));
 
 // Error Handler Middleware
 app.use(errorHandler);
 
+// Routes
+app.use('/api/projects', require('./routes/projectRoutes'));
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
